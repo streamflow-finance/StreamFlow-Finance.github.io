6aef562525a0809c67003c5d29ec5201b862ac7e
imprfekt <5047969+imprfekt@users.noreply.github.com>
2021-05-24 23:25:11 +0200

    push production build

---
+++ app/build/precache-manifest.3acf0e10491e25054816d09223b91fb9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "369e42254dd1c6979f877942ddc31da9",
+    "url": "../public/index.html"
+  },
+  {
+    "revision": "15eaa3f6147b8a80c81f",
+    "url": "../public/static/css/main.0f76be15.chunk.css"
+  },
+  {
+    "revision": "6f7e634b85f35b6c9882",
+    "url": "../public/static/js/2.30a0a6fb.chunk.js"
+  },
+  {
+    "revision": "c2a8c94b307584fb58199b77387029c9",
+    "url": "../public/static/js/2.30a0a6fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15eaa3f6147b8a80c81f",
+    "url": "../public/static/js/main.350a4b53.chunk.js"
+  },
+  {
+    "revision": "53f5552847e2c6992aec",
+    "url": "../public/static/js/runtime-main.769f2791.js"
+  },
+  {
+    "revision": "a4e80c1e977416ec45d808a3268892b7",
+    "url": "../public/static/media/builton.a4e80c1e.svg"
+  }
+]);
