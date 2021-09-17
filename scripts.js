3f43d3f99b9fd9042a37f1f9904f4884d1d9c821
imprfekt <5047969+imprfekt@users.noreply.github.com>
2021-09-17 17:46:39 +0200

    Added a roadmap and "careers" section

--- scripts.js
+++ scripts.js
@@ -1,1 +1,2 @@
 document.getElementById("mail").setAttribute("href", "mai" + "lto:he" + "llo@strea" + "mflow.fin" + "ance");
+document.getElementById("work").setAttribute("href", "mai" + "lto:wor" + "k@strea" + "mflow.fin" + "ance");
