eb63095acd2cae184e8542720022cfb5fee4c60d
Dušan <trajkodulic@gmail.com>
2022-11-30 18:29:24 +0100

    Home page and contact
    Changed homepage footer and other changes from figma.
    
    Added contact page

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,14 @@ module.exports = {
     fontFamily: {
       inter: ["inter", "sans-serif"],
     },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+      "3xl": "1920px",
+    },
     extend: {
       colors: {
         primary: "var(--primary)",
