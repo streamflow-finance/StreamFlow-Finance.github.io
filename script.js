d19825c55f9d3b4f72c893a2eb95df7391c65079
Dušan <trajkodulic@gmail.com>
2022-12-01 16:59:30 +0100

    Removed script.js
    Not needed anymore since we submit form instead of reaching to mail now

--- script.js
+++
@@ -1,7 +0,0 @@
-function mailFunctions() {
-    document.getElementById("mail").setAttribute("href", "mai" + "lto:he" + "llo@strea" + "mflow.fin" + "ance");
-    //document.getElementById("work").setAttribute("href", "mai" + "lto:wor" + "k@strea" + "mflow.fin" + "ance");
-    //console.log("i work");
-  }
-
-  window.onload = mailFunctions;
