c82144183155047dc996903ef7612d51a8788c69
Dushan <trajkodulic@gmail.com>
2023-03-01 15:03:54 +0100

    Updated testimonials and tweets

--- slider.js
+++ slider.js
@@ -1,16 +1,16 @@
 $(function () {
-  $(".slider").slick({
+  $('.slider').slick({
     speed: 400,
-    slidesToShow: 6,
+    slidesToShow: 4,
     slidesToScroll: 1,
     variableWidth: true,
     prevArrow:
       '<div><img src="imgs/prev-arrow.png" class="prev-arrow cursor-pointer" /></div>;',
     nextArrow:
       '<div><img src="imgs/next-arrow.png" class="next-arrow cursor-pointer"/></div>;',
-  });
+  })
 
-  $(".img-slider").slick({
+  $('.img-slider').slick({
     speed: 400,
     slidesToShow: 2,
     slidesToScroll: 1,
@@ -19,5 +19,5 @@ $(function () {
       '<div><img src="imgs/prev-arrow.png" class="prev-arrow2 cursor-pointer"/></div>;',
     nextArrow:
       '<div><img src="imgs/next-arrow.png" class="next-arrow2 cursor-pointer"/></div>;',
-  });
-});
+  })
+})
